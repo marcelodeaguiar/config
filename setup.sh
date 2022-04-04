2bd9a93fb7bacdb9764cee14ffa9f5f5215f37b2
Marcelo Aguiar <picpay@Picpays-MacBook-Air-3.local>
2022-04-04 17:11:06 -0300

    Fixes

--- setup.sh
+++ setup.sh
@@ -1,28 +1,38 @@
 # Stop on failure
-set -e
+#set -e
 
 # Install oh-my-zsh
-sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+if [ ! -d ~/.oh-my-zsh ]; then
+    sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+    git clone https://github.com/powerline/fonts.git
+    ./fonts/install.sh
+    rm -rf fonts
+fi
 
 # Install homebrew
 which -s brew
 if [[ $? != 0 ]] ; then
     # Install Homebrew
-    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+    bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+    echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> ~/.zprofile
+    eval "$(/opt/homebrew/bin/brew shellenv)"
 else
     brew update
 fi
 
 # install brew apps
-brew bundle --file=- <<-EOS
-brew rbenv ruby-build
-brew keepassxc
-brew ctags
-brew gh
-brew git-lfs
-brew jq
-brew loc
-brew tmux
-brew vim
-EOS
+brew bundle --file=~/config/setup-brewfile
 
+# Fix config files
+convertToLink () {
+	local home=~
+	local config="$home/config"
+	local p="$home/$1"
+
+	[[ -f "$p" || -L "$p" ]] && rm "$p"
+	ln -s "$config/$1" $p
+}
+
+convertToLink ".vimrc" 
+convertToLink ".zshrc" 
+convertToLink ".xvimrc" 
